9fd4512331192593c364e65f4e361ed556aa13eb
premdeep909 <premdeep909@gmail.com>
2024-04-19 10:12:04 +0530

    Adding Middleware

--- App.js
+++ App.js
@@ -3,6 +3,19 @@ const http = require('http');
 
 const express = require('express');
 const handle = express();
+handle.use((req,res,next) =>{
+console.warn("middleware-1");
+next();
+})
+handle.use((req,res,next) =>{
+    console.warn("middleware-2");
+    next();
+    })
+    handle.use((req,res,next) =>{
+        console.warn("middleware-3");
+  //send respomse 
+  res.send('<h1>Hello From Express JS </h1>')
+        })
 const server = http.createServer(handle); 
 server.listen(3000);
 

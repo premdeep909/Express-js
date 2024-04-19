62acc8f443a5de9168413e9f01601d10699155c4
premdeep909 <premdeep909@gmail.com>
2024-04-19 11:52:55 +0530

    Parsing Incoming Requests

--- App.js
+++ App.js
@@ -2,25 +2,23 @@
 const http = require('http');
 
 const express = require('express');
+const bodyparser = require('body-parser');
 const handle = express();
 
-handle.use('/',(req,res,next) =>{
-    console.warn("middleware-1");
-//send respomse 
-next();
-    })
-
+handle.use(bodyparser.urlencoded({extended:false}))
 
     handle.use('/add-users',(req,res,next) =>{
         console.warn("middleware-2");
-  //send respomse 
-  res.send('<h1>Hello From Express JS </h1>')
+        res.send('<h1><form action="/users" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form></h1>')
         })
+      handle.post('/users',(req,res,next)=>{
+        console.log(req.body);
+        res.redirect('/');
+      })
 
         handle.use('/',(req,res,next) =>{
             console.warn("middleware-3");    
-        //send respomse 
-        res.send('<h1>Hello</h1>')
+            res.send('<h1>Hello</h1>')
             })
         
 const server = http.createServer(handle); 

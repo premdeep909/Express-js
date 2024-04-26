7a5ff4de5eea08f8ec49a68c8f9b99d0363a8678
premdeep909 <premdeep909@gmail.com>
2024-04-26 10:05:08 +0530

    Serving static files

--- App.js
+++ App.js
@@ -5,20 +5,23 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const handle = express();
 const path = require('path');
+const viewPath = require('./utils/Path');
 
 
+handle.use(bodyparser.urlencoded({extended:false}));
+handle.use(express.static(path.join(__dirname,'public')));
 
 const adminRoutes = require('./Routes/admin');
 const shopRoutes = require('./Routes/shop');
 
-handle.use(bodyparser.urlencoded({extended:false}));
+
 handle.use('/admin',adminRoutes);
 handle.use(shopRoutes);
 
 handle.use((req,res,next) =>{
-    res.status(404).sendFile(path.join(__dirname,'views','Page-Not-Found.html'));
+    res.status(404).sendFile(viewPath('Page-Not-Found.html'));
 })
         
 const server = http.createServer(handle); 
-server.listen(3000);
+server.listen(3005);
 

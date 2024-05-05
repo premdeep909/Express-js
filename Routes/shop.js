a3ef88ed514be07133a332830f27271cbad7b540
premdeep909 <premdeep909@gmail.com>
2024-05-05 15:46:34 +0530

    Converting Project to Handlebars

--- Routes/shop.js
+++ Routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // Path + method
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
-  res.render('shop',{prod: adminData.products,pageTitle: "shop",path:"/"});
+  res.render('shop',{prod: adminData.products,pageTitle: "shop",path:"/", hasProduct: adminData.products.length>0});
 });
 
 module.exports = router;

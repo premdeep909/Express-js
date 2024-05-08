4742466e6a5609523397dbdb09152b58b5ae9a80
premdeep909 <premdeep909@gmail.com>
2024-05-08 11:08:13 +0530

    Refactoring Product Model

--- models/product.js
+++ models/product.js
@@ -1,31 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+const pathBuilt = path.join(path.dirname(require.main.filename),"data","product.json");
+const getProductFromFile = (cb) =>{
+    fs.readFile(pathBuilt,(err,fileContent) =>{
+        if(err){
+           return cb([]);
+        }
+       cb(JSON.parse(fileContent));
+  })
+}
 module.exports = class Product{
     constructor(IncomingTitle){
       this.title = IncomingTitle;
     }
     save(){
-        const pathBuilt = path.join(path.dirname(require.main.filename),"data","product.json");
-        fs.readFile(pathBuilt,(err,fileContent) =>{
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile((products) =>{
             products.push(this);
-
             fs.writeFile(pathBuilt,JSON.stringify(products),(err) =>{
                 console.log('err',err);
             })
         })
+      
     }
     static fetchAll(cb){
-        const pathBuilt = path.join(path.dirname(require.main.filename),"data","product.json");
-        fs.readFile(pathBuilt,(err,fileContent) =>{
-            if(err){
-                cb([]);
-            }
-             cb(JSON.parse(fileContent));
-        })
+        getProductFromFile(cb);
     }
 }
